d9c3462e28a8cafc6f981ec3ee48afb80be3d8ea
admin-on-win10 <shashwatjaiswal14@gmail.com>
2023-04-18 08:47:16 +0530

    initialized with tailwind

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom/client'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h2>Your mom</h2>
+    <h2 className='bg-slate-400'>Works</h2>
   </React.StrictMode>,
 )
