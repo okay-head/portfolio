33910db63aa11437b34f819a168767c390905faa
admin-on-win10 <shashwatjaiswal14@gmail.com>
2023-04-22 21:05:08 +0530

    progress on section 2

---
+++ src/components/skills/SkillCard.jsx
@@ -0,0 +1,9 @@
+export default function SkillCard({source='./../../../src/assets/brand/icons8-css3-480.png'}) {
+  return (
+    <div className="bg-transparent my-4
+    
+    ">
+      <img src={source} alt="cssIcon" className='w-[60%] mx-auto' />
+    </div>
+  )
+}
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,68 +1,26 @@
 import Wrapper from '../Wrapper'
+import SkillCard from './SkillCard'
 export default function Skills() {
+  const pth = './../../../src/assets/brand/'
 	return (
-		<div className='skills bg-[var(--bg-primary)] py-20'>
+		<div className='skills bg-[var(--bg-primary)] py-16 rounded-t-3xl -mt-6'>
       <Wrapper>
-      <h1 className='text-4xl text-center pb-2'>Skills</h1>
-				<div>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, ducimus
-					reiciendis, voluptatem illo quod dolorum perspiciatis voluptate,
-					expedita blanditiis sed officia natus laborum repellat asperiores enim
-					nam quidem molestias rerum minima. Aliquam quo sunt error blanditiis
-					suscipit impedit animi pariatur velit, praesentium quod similique
-					cupiditate, exercitationem perferendis, maxime quas assumenda nihil
-					iste quisquam est quae? Ratione ipsam sed repellat sit incidunt, nulla
-					obcaecati aliquam. Inventore, fuga obcaecati architecto quis ab quam.
-					Aliquid temporibus asperiores sint quibusdam, doloremque soluta
-					reprehenderit eligendi? Iste, libero error? Adipisci repellat
-					veritatis accusantium in, veniam sequi rem, ad recusandae, eligendi
-					amet consequatur totam. Praesentium amet ipsa neque alias totam esse,
-					dolorem labore earum officiis commodi facere aperiam qui dolor
-					cupiditate. Sint nam rem corrupti blanditiis, est hic exercitationem
-					magni labore, ad itaque consectetur deserunt delectus suscipit dolore,
-					asperiores id quis? Autem cupiditate in voluptas sit sapiente fuga
-					praesentium, unde impedit, repellat corporis illum incidunt modi, illo
-					similique deserunt iusto placeat ducimus! Placeat, quidem mollitia
-					eligendi explicabo debitis inventore id fuga autem qui cumque
-					voluptas, iste et! Temporibus, corporis quisquam maxime nam architecto
-					eos asperiores doloribus non repellat debitis eaque dolorem cumque qui
-					error incidunt ea harum earum. Culpa et quo, quisquam natus quae
-					mollitia consequuntur aspernatur impedit illo cumque possimus ut
-					pariatur, quod id, eligendi suscipit minus nihil ratione ipsa
-					voluptates quidem perferendis? Similique sed delectus quaerat tempore
-					hic beatae, quos excepturi, necessitatibus culpa illum vel.
-					Voluptates, architecto excepturi reiciendis id, eius pariatur amet ab
-					libero quasi, laudantium nisi quidem officia provident ea voluptatum
-					unde quae aperiam nemo placeat adipisci quod quos optio. Repellendus
-					voluptatem odit quisquam suscipit praesentium deleniti, nesciunt eius
-					eaque voluptates maxime mollitia corrupti eos rem iste incidunt labore
-					iure blanditiis nemo explicabo beatae illo! Unde numquam vero earum
-					laborum tempore soluta! Possimus, totam ad in voluptates praesentium
-					enim neque asperiores earum velit sed explicabo ratione tempore, quos
-					optio. Vel quos voluptatibus incidunt, distinctio aliquid ratione
-					accusantium consequatur expedita id laudantium possimus repellat
-					itaque unde non explicabo a maxime ullam molestias similique
-					cupiditate tempore! Dolore numquam delectus facere cupiditate,
-					architecto sed nobis odit iure laborum praesentium magnam ipsum eius
-					ut blanditiis veritatis in aspernatur sunt repellat error cumque
-					inventore distinctio corrupti eligendi. Quos quisquam iste blanditiis
-					fugiat dolorum voluptates eligendi, ratione magni dolorem repudiandae
-					corrupti quaerat libero ex officia quo non odit vel possimus ducimus,
-					consectetur velit illum saepe doloribus? Perspiciatis excepturi
-					reprehenderit illum necessitatibus. Beatae, nemo voluptas. Deserunt
-					rem consequatur unde in autem. Tenetur recusandae quas odio. Ex, ab
-					quod! Reiciendis iure beatae impedit possimus, fuga quidem dolore
-					mollitia quod culpa, iusto ipsam quae aspernatur. Qui ullam saepe
-					numquam labore enim accusamus modi eum officiis adipisci repudiandae
-					reprehenderit consequuntur quia illo corrupti ut veniam eius, quo at.
-					Fugiat quidem pariatur enim dolorem in vel aliquid. Recusandae omnis
-					nisi accusantium corrupti porro fugit quae voluptates sapiente magni
-					perferendis, ut quam quasi vitae cupiditate sint assumenda consequatur
-					quaerat, temporibus molestias alias. Aut exercitationem labore aliquid
-					facilis provident sunt soluta, hic, nobis officia ipsam quasi ab
-					dolorem cum, voluptatibus temporibus repellat pariatur fugiat esse
-					assumenda.
-				</div>
+      <h1 className='text-4xl text-center mb-10'>Skills</h1>    
+				<div className=" mx-auto max-w-5xl 
+        md:grid md:grid-cols-2 lg:grid-cols-3">
+        <SkillCard source={pth+'icons8-html-5-480.png'}/>
+        <SkillCard source={pth+'icons8-css3-480.png'}/>
+        <SkillCard source={pth+'icons8-javascript-480.png'}/>
+        <SkillCard source={pth+'react-1-logo-svg-vector.svg'}/>
+        <SkillCard source={pth+'icons8-tailwind-css-480.png'}/>
+        <SkillCard source={pth+'icons8-bootstrap-480.png'}/>
+        <SkillCard source={pth+'icons8-firebase-480.png'}/>
+        <SkillCard source={pth+'jquery_vertical_logo_icon_169489.png'}/>
+        <SkillCard source={pth+'icons8-amazon-web-services-480(1).png'}/>
+        <SkillCard source={pth+'nodejs-1-logo.png'}/>
+        <SkillCard source={pth+'png-transparent-mongodb-original-wordmark-logo-icon-thumbnail-removebg-preview.png'}/>
+        <SkillCard source={pth+'icons8-figma-480.png'}/>
+        </div>
       </Wrapper>
 		</div>
 	)
