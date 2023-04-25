c92f9f8cbddb9659e22af38277ce8d45d241d7d3
admin-on-win10 <shashwatjaiswal14@gmail.com>
2023-04-25 23:13:24 +0530

    form actions to be added| firebase integration

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,24 +3,45 @@ export default function Contact() {
 	return (
 		<div className='contact bg-[var(--bg-purple)]  py-[var(--p-section-y)]'>
 			<Wrapper>
-				<h1 className='text-5xl text-center mb-10 md:mb-20'>Contact</h1>
-				<div className='mx-8
+				<h1 className='text-5xl text-center mb-10 md:mb-15'>Contact</h1>
+				<div
+					className='mx-8
 			md:grid grid-cols-2 items-center lg:mx-auto'
 				>
 					<div>
-            <h2 className='w-1/2 mx-auto text-left text-6xl text-[black] text-gradient py-2 '>Let's work together</h2>
-            <img src="./../../src/assets/th-2066431090-removebg-preview.png" alt="Waving astronaut" className='hue-rotate-30 mx-auto' />
-            </div>
-    
-          <div className='py-4'>
-            <form action="">
-            <input type="text" name="text" className='input-style' placeholder='Your name' />
-            <input type="email" name="email" className='input-style' placeholder='Your email' />
+						<img
+							src='./../../src/assets/th-2066431090-removebg-preview.png'
+							alt='Waving astronaut'
+							className='hue-rotate-30 mx-auto w-3/4'
+						/>
+					</div>
 
-            <textarea name="textarea" placeholder='Your irrelevant message' className='input-style min-h-[280px]'></textarea>
-  
-            </form>
-          </div>
+					<div className='py-4'>
+						<h2 className='mx-auto text-left text-4xl text-[black] text-gradient py-3 mb-5 ms-2'>
+							Let's work together!
+						</h2>
+						<form action=''>
+							<input
+								type='text'
+								name='text'
+								className='input-style'
+								placeholder='Your name'
+							/>
+							<input
+								type='email'
+								name='email'
+								className='input-style'
+								placeholder='Your email'
+							/>
+
+							<textarea
+								name='textarea'
+								placeholder='Your irrelevant message'
+								className='input-style min-h-[250px]'
+							></textarea>
+              <button id='submit' className='bg-slate-500 rounded-md border-1px border-black py-2 px-4 mx-auto block'>Send</button>
+						</form>
+					</div>
 				</div>
 			</Wrapper>
 		</div>
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -9,7 +9,7 @@ export default function Projects() {
 				<div className="md:grid grid-cols-2 auto-rows-fr">
           <Card source={'./../../../src/assets/github-finder-II.png'} alt={'Github finder'} caption={'Github finder'} live_url={'https://okay-head-github-finder.netlify.app/'} code_url={'https://github.com/okay-head/Github-Finder'}/> 
 
-          <Card source={'./../../../src/assets/ableton-snapshot-home-IIII.png'} alt={'Ableton'} caption={'Ableton'} cls={'row-span-2'} live_url={'https://okay-head.github.io/Ableton/'} code_url={'https://github.com/okay-head/Ableton'}/>
+          <Card source={'./../../../src/assets/ableton-snapshot-home-IIII.png'} alt={'Ableton'} caption={'Ableton (responsive)'} cls={'row-span-2'} live_url={'https://okay-head.github.io/Ableton/'} code_url={'https://github.com/okay-head/Ableton'}/>
 
           <Card source={'./../../../src/assets/gradient-generator-snapshot-II.png'} alt={'Gradient generator'} caption={'Gradient generator'} code_url={'https://github.com/okay-head/Gradient-generator'} live_url={'https://okay-head.github.io/Gradient-generator/'}/>
         </div>
