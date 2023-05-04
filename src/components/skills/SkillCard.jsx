ff668d3657f576aeba644c1eb8ae41fd109cbf5c
admin-on-win10 <shashwatjaiswal14@gmail.com>
2023-05-04 21:11:18 +0530

    Refactoring

--- src/components/skills/SkillCard.jsx
+++ src/components/skills/SkillCard.jsx
@@ -1,19 +1,8 @@
 import {motion as m} from 'framer-motion'
-export default function SkillCard({source='/assets/brand/icons8-css3-480.png', variants, title, classVars=''}) {
-/*   const item = {
-    hidden:{
-      opacity: 0,
-      x: -30
-    },
-    show:{
-      opacity: 1,
-      x: 0,
-    },
-    transition:{duration: 0.5}
-  } */
+export default function SkillCard({path='/assets/brand/icons8-css3-480.png', variants, title, classVars=''}) {
   return (
     <m.div variants={variants} title={title} className={`bg-transparent my-4 ${classVars}`}>
-      <img src={source} alt="cssIcon" className='w-[55%] mx-auto' />
+      <img src={path} alt={`${title} icon`} className='w-[55%] mx-auto' />
     </m.div>
   )
 }
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -2,17 +2,43 @@ import { motion as m } from 'framer-motion'
 import Wrapper from '../Wrapper'
 import SkillCard from './SkillCard'
 export default function Skills() {
-	const pth = '././assets/brand/'
+	const pth = '/assets/brand/'
+
+	const abc = [{ title: 'HTML', path: '/assets/brand/icons8-html-5-480.png' }]
+
+	const imgArr = [
+		{ title: 'HTML', path: '/assets/brand/icons8-html-5-480.png' },
+		{ title: 'CSS', path: '/assets/brand/icons8-css3-480.png' },
+		{ title: 'JS', path: '/assets/brand/icons8-javascript-480.png' },
+		{
+			title: 'React',
+			path: '/assets/brand/react-1-logo-png-transparent-removebg-preview.png',
+		},
+		{ title: 'Tailwind', path: '/assets/brand/icons8-tailwind-css-480.png' },
+		{ title: 'Bootstrap', path: '/assets/brand/icons8-bootstrap-480.png' },
+		{ title: 'Firebase', path: '/assets/brand/icons8-firebase-480.png' },
+		{ title: 'jQuery', path: '/assets/brand/jquery-icon.png' },
+		{
+			title: 'AWS',
+			path: '/assets/brand/icons8-amazon-web-services-480(1).png',
+		},
+		{ title: 'Node', path: '/assets/brand/nodejs.png' },
+		{
+			title: 'MongoDb',
+			path: '/assets/brand/erkxwhl1gd48xfhe2yld-removebg-preview.png',
+		},
+		{ title: 'Scss', path: '/assets/icons8-sass-avatar-480.png' },
+	]
 
 	const cardVariants = {
 		initial: { opacity: 0, x: '-3rem' },
 		view: {
 			opacity: 1,
 			x: 0,
-      transition: { staggerChildren: 0.1, ease: 'easeInOut', duration: 0.8 },
+			transition: { staggerChildren: 0.1, ease: 'easeInOut', duration: 0.8 },
 		},
 	}
-  
+
 	return (
 		// view port units for x axis padding?
 		<div
@@ -21,55 +47,38 @@ export default function Skills() {
 		>
 			<Wrapper>
 				<div className='mb-10'>
-					<m.h1 
-          initial = {{y:'100%', opacity: 0}}
-          whileInView = {{ y:'0%', opacity: 1}}
-          viewport={{ once: true, amount: 0.8 }}
-          transition={{delay: 0, duration:0.4}}
-          className='text-5xl text-center'>Skills</m.h1>
+					<m.h1
+						initial={{ y: '100%', opacity: 0 }}
+						whileInView={{ y: '0%', opacity: 1 }}
+						viewport={{ once: true, amount: 0.8 }}
+						transition={{ delay: 0, duration: 0.4 }}
+						className='text-5xl text-center'
+					>
+						Skills
+					</m.h1>
 				</div>
 				<div className='text-center text-xl mb-5 italic'>
 					Technologies I've worked with
 				</div>
 
 				<m.div
-          variants={cardVariants}
-          initial = 'initial'
-          whileInView='view'
-          viewport={{ once: true, amount: 0.3 }}
+					variants={cardVariants}
+					initial='initial'
+					whileInView='view'
+					viewport={{ once: true, amount: 0.3 }}
 					className=' mx-auto max-w-4xl
         sm:grid sm:grid-cols-3 lg:grid-cols-4'
 				>
-					<SkillCard variants={cardVariants} title='HTML' source={pth + 'icons8-html-5-480.png'} />
-					<SkillCard variants={cardVariants} title='CSS' source={pth + 'icons8-css3-480.png'} />
-					<SkillCard variants={cardVariants} title='JS' source={pth + 'icons8-javascript-480.png'} />
-					<SkillCard variants={cardVariants}
-						title='React'
-						source={pth + 'react-1-logo-png-transparent-removebg-preview.png'}
-					/>
-					<SkillCard variants={cardVariants}
-						title='Tailwind'
-						source={pth + 'icons8-tailwind-css-480.png'}
-					/>
-					<SkillCard variants={cardVariants}
-						title='Bootstrap'
-						source={pth + 'icons8-bootstrap-480.png'}
-					/>
-					<SkillCard variants={cardVariants}
-						title='Firebase'
-						source={pth + 'icons8-firebase-480.png'}
-					/>
-					<SkillCard variants={cardVariants} title='jQuery' source={pth + 'jquery-icon.png'} />
-					<SkillCard variants={cardVariants}
-						title='AWS'
-						source={pth + 'icons8-amazon-web-services-480(1).png'}
-					/>
-					<SkillCard variants={cardVariants} title='Node' source={pth + 'nodejs.png'} classVars='hue' />
-					<SkillCard variants={cardVariants}
-						title='MongoDb'
-						source={pth + 'erkxwhl1gd48xfhe2yld-removebg-preview.png'}
-					/>
-					<SkillCard variants={cardVariants} title='Figma' source={pth + 'icons8-figma-480.png'} />
+					{imgArr.map(({ title, path }, i) => {
+						return (
+							<SkillCard
+								key={i * i}
+								variants={cardVariants}
+								title={title}
+								path={path}
+							/>
+						)
+					})}
 				</m.div>
 			</Wrapper>
 		</div>
