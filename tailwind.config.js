5950f1cdf30817c24ef6e1e1cb6886498d1088f9
admin-on-win10 <shashwatjaiswal14@gmail.com>
2023-05-21 16:30:51 +0530

    refactoring

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
     extend: {
-        fontFamily: {
-          'monsterrat': ['Montserrat', 'sans-serif'],
-          'poppins': ['Poppins', 'sans-serif'],
-          'source': ['Source Sans 3', 'sans-serif'],
-          'noto': ['Noto Sans', 'sans-serif'],
-          'DM': ['DM Sans', 'sans-serif'],
-          'alef': ['Alef', 'sans-serif'],
-          'open': ['Open Sans', 'sans-serif'],
-          'lato': ['Lato', 'sans-serif'],
-          'rubik': ['Rubik', 'sans-serif'],
+      fontFamily: {
+        'monsterrat': ['Montserrat', 'sans-serif'],
+        'poppins': ['Poppins', 'sans-serif'],
+        'source': ['Source Sans 3', 'sans-serif'],
+        'noto': ['Noto Sans', 'sans-serif'],
+        'DM': ['DM Sans', 'sans-serif'],
+        'alef': ['Alef', 'sans-serif'],
+        'open': ['Open Sans', 'sans-serif'],
+        'lato': ['Lato', 'sans-serif'],
+        'rubik': ['Rubik', 'sans-serif'],
+      },
     },
-  },
-  plugins: [],
-}
+    plugins: [],
+  }
 }
