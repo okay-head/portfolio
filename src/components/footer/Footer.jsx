ea0bd74e08f44eda73d4a23179876e68d2d8cea4
admin-on-win10 <shashwatjaiswal14@gmail.com>
2023-05-28 21:06:01 +0530

    refactoring

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,19 +1,19 @@
 import Wrapper from '../Wrapper'
 import Social from './Social'
 
-export default function Footer() {
-	return (
-		<footer className=' bg-black text-center sm:text-left text-lg font-poppins tracking-wide'>
-			<Wrapper classVars={'sm:grid grid-cols-2 py-10 items-center'}>
-				<div>
-					<h2 className='font-bold text-xl py-2 mb-4 sm:mb-0'>PORTFOLIO</h2>
-				</div>
-				<div className='ms-auto'>
-					<Social url='https://medium.com/@shashwat-jaiswal' />
-					<Social url='https://github.com/okay-head' />
-					<Social url='https://twitter.com/shash0_0' />
-				</div>
-			</Wrapper>
-		</footer>
-	)
+export default function Footer () {
+  return (
+    <footer className=' bg-black text-center sm:text-left text-lg font-poppins tracking-wide'>
+      <Wrapper classVars={'sm:grid grid-cols-2 py-10 items-center'}>
+        <div>
+          <h2 className='font-bold text-xl py-2 mb-4 sm:mb-0'>PORTFOLIO</h2>
+        </div>
+        <div className='ms-auto'>
+          <Social url='https://medium.com/@shashwat-jaiswal' />
+          <Social url='https://github.com/okay-head' />
+          <Social url='https://twitter.com/shash0_0' />
+        </div>
+      </Wrapper>
+    </footer>
+  )
 }
