aa6312cf9b67547b49558f7a6b9242be62011f62
admin-on-win10 <shashwatjaiswal14@gmail.com>
2023-05-31 21:27:52 +0530

    refactoring

--- src/components/navbar/Anchor.jsx
+++ src/components/navbar/Anchor.jsx
@@ -1,27 +1,27 @@
 import { useContext } from 'react'
 import NavContext from './NavContextProvider'
 
-export default function Anchor({
-	children,
-	linkTo,
-	classVars,
-	imgVars,
-	flag = false,
+export default function Anchor ({
+  children,
+  linkTo,
+  classVars,
+  imgVars,
+  flag = false
 }) {
-	const { disabled, setDisabled } = useContext(NavContext)
+  const { disabled, setDisabled } = useContext(NavContext)
 
-	return (
-		<a
-			onClick={() => setDisabled((prev) => !prev)}
-			href={'#' + linkTo}
-			className={`mx-4 relative group ${classVars}`}
-		>
-			{children}
-			<img
-				className={`group-hover:opacity-100 group-hover:translate-y-0 w-5 absolute inset-0 top-[-80%] left-1/4 transition-transform duration-150 translate-y-1 ease-in-out opacity-0 ${imgVars}`}
-				src='/assets/th-2939161216-removebg-preview.png'
-				alt='saturn'
-			/>
-		</a>
-	)
+  return (
+    <a
+      onClick={() => setDisabled(prev => !prev)}
+      href={'#' + linkTo}
+      className={`mx-4 relative group ${classVars}`}
+    >
+      {children}
+      <img
+        className={`group-hover:opacity-100 group-hover:translate-y-0 w-5 absolute inset-0 top-[-80%] left-1/4 transition-transform duration-150 translate-y-1 ease-in-out opacity-0 ${imgVars}`}
+        src='/assets/th-2939161216-removebg-preview.png'
+        alt='saturn'
+      />
+    </a>
+  )
 }
