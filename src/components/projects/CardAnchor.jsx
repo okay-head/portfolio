cb9ca54561fc117a156ea3bbcfe6282dee9b6eaf
admin-on-win10 <shashwatjaiswal14@gmail.com>
2023-06-03 19:58:08 +0530

    refactoring

--- src/components/projects/CardAnchor.jsx
+++ src/components/projects/CardAnchor.jsx
@@ -1,11 +1,12 @@
-export default function CardAnchor({ url, source, alt }) {
-	return (
-		<a href={url} target='_blank' className='cardAnchor mx-3'>
-			<img loading='lazy'
-				src={source}
-				alt={alt}
-				className='w-[45px] p-2 rounded-[50%] bg-slate-50'
-			/>
-		</a>
-	)
+export default function CardAnchor ({ url, source, alt }) {
+  return (
+    <a href={url} target='_blank' className='cardAnchor mx-3'>
+      <img
+        loading='lazy'
+        src={source}
+        alt={alt}
+        className='w-[45px] p-2 rounded-[50%] bg-slate-50'
+      />
+    </a>
+  )
 }
