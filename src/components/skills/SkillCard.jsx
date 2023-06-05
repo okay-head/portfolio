e430217e80cb7fd016985a51e3fe1920142c3d01
admin-on-win10 <shashwatjaiswal14@gmail.com>
2023-06-05 10:24:01 +0530

    refactoring

--- src/components/skills/SkillCard.jsx
+++ src/components/skills/SkillCard.jsx
@@ -1,7 +1,16 @@
-import {motion as m} from 'framer-motion'
-export default function SkillCard({path='/assets/brand/icons8-css3-480.png', variants, title, classVars=''}) {
+import { motion as m } from 'framer-motion'
+export default function SkillCard ({
+  path = '/assets/brand/icons8-css3-480.png',
+  variants,
+  title,
+  classVars = ''
+}) {
   return (
-    <m.div variants={variants} title={title} className={`bg-transparent my-4 ${classVars}`}>
+    <m.div
+      variants={variants}
+      title={title}
+      className={`bg-transparent my-4 ${classVars}`}
+    >
       <img src={path} alt={`${title} icon`} className='w-[55%] mx-auto' />
     </m.div>
   )
