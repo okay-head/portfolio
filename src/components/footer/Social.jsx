8f46a77c14a91889f5a0e955521b3e3bce951978
okay-head <68184623+okay-head@users.noreply.github.com>
2023-10-25 14:59:54 +0530

    removed border from footer

--- src/components/footer/Social.jsx
+++ src/components/footer/Social.jsx
@@ -5,7 +5,7 @@ export default function Social ({ url, classVars }) {
       url={url}
       bgColor='transparent'
       fgColor='#f8fafc'
-      className={`social mx-2 border-2 border-[#f8fafc] rounded-[50%] ${classVars}`}
+      className={`social mx-2 border-[0px] border-[#f8fafc] rounded-[50%] ${classVars}`}
     />
   )
 }
