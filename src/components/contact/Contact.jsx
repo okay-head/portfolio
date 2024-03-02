be987fdb8fb6f52381bfe9b92f34e20618c5149d
okay-head <68184623+okay-head@users.noreply.github.com>
2024-03-02 19:58:26 +0530

    style: replace home image|add link to repo

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -33,7 +33,27 @@ export default function Contact() {
       <Wrapper>
         <H1 classVars='mb-4'>Contact</H1>
         <div className='text-center lg:text-lg mb-10 text-[var(--white-2)] max-w-4xl  mx-auto'>
-          If you like my work, consider adding a 🌟 on github. <br />
+          If you like my work, consider adding a
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 24 24'
+            fill='currentColor'
+            class='w-5 h-5 inline-block ms-2 me-2 mb-1'
+          >
+            <path
+              fill-rule='evenodd'
+              d='M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z'
+              clip-rule='evenodd'
+            />
+          </svg>
+          on{' '}
+          <a
+            href='https://github.com/okay-head/portfolio'
+            className='underline hover:no-underline inline-block'
+          >
+            Github.
+          </a>{' '}
+          <br />
           You can also contact me via mail or twitter.
         </div>
         <div
